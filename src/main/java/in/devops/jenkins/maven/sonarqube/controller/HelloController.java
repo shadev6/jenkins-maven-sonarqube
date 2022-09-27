32569cb8cdd8429383d52cfd0ce863db9027c96e
Keyon Genesis Kanan <keyongenesis@gmail.com>
2022-09-27 13:52:51 +0800

    New files

---
+++ src/main/java/in/devops/jenkins/maven/sonarqube/controller/HelloController.java
@@ -0,0 +1,28 @@
+package in.devops.jenkins.maven.sonarqube.controller;
+import in.devops.jenkins.maven.sonarqube.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.*;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    private String instance = "Test-Machine";
+    private String name = "Welcome to Automated JENKINS DEPLOYMENT to K8S";
+
+    @Autowired
+    private HelloService hs;
+
+    @GetMapping("/health")
+    public String health(){
+        return hs.getHealth(instance);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return hs.getHello(name);
+    }
+}
